c4b6c7216acd9bb0648ae12046084a0ff686f9a0
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-10-16 17:25:04 +0700

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal{
+    @Override
+    public void eat() {
+        System.out.println("Meat");
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("water");
+    }
+}
